a4251f6d131013dbc65f8cf6cc7094772fb94176
MohandHisham <mohandhisham4@gmail.com>
2025-11-23 15:47:29 +0200

    edit in route.js

--- assets/assets.js
+++ assets/assets.js
@@ -107,27 +107,28 @@ export const serviceData = [
   {
     icon: assets.web_icon,
     title: "Web design",
-    description: "Web development is the process of building, programming...",
+    description:
+      "Building interactive web applications using React and Next.js...",
     link: "",
   },
   {
     icon: assets.mobile_icon,
-    title: "Mobile app",
-    description:
-      "Mobile app development involves creating software for mobile devices...",
+    title: "Responsive Design",
+    description: "Creating websites that work seamlessly across all devices",
     link: "",
   },
   {
     icon: assets.ui_icon,
-    title: "UI/UX design",
+    title: "Performance Optimization",
     description:
-      "UI/UX design focuses on creating a seamless user experience...",
+      "Optimizing web applications for speed, SEO, and user experience...",
     link: "",
   },
   {
     icon: assets.graphics_icon,
-    title: "Graphics design",
-    description: "Creative design solutions to enhance visual communication...",
+    title: "API Integration ",
+    description:
+      "Connecting frontends with backend services and third-party APIs",
     link: "",
   },
 ];
